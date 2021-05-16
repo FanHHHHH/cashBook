ed4763d71a36e0317ecc029216adbd029dd2bb86
hef <hefan813@vip.qq.com>
2021-05-16 19:09:59 +0800

    完成NumberPadSSection组件

--- src/Money/NumberPadSection.tsx
+++ src/Money/NumberPadSection.tsx
@@ -1,9 +1,13 @@
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   > .output {
+    display: flex;
+    justify-content: flex-end;
+    overflow: auto;
     background: #fff;
     padding: 0 16px;
     line-height: 72px;
@@ -56,4 +60,64 @@ const NumberPadSection = styled.section`
     }
   }
 `
+
+const NumberPadSection: React.FC = () => {
+  const [number, _setNumber] = useState<string>('0')
+  const setNumber = (num: string) => {
+    if (number.length === 16) {
+      return
+    }
+    _setNumber(num)
+  }
+  const onClick = (e: React.MouseEvent) => {
+    const num = (e.target as HTMLButtonElement).textContent
+    if (num === null) {
+      return
+    }
+    if (num === '0' || num === '1' || num === '2' || num === '3' || num === '4' || num === '5' || num === '6' || num === '7' || num === '8' || num === '9') {
+      if (number === '0') {
+        setNumber(num)
+      } else {
+        setNumber(number + num)
+      }
+    } else if (num === '.') {
+      const n = number.indexOf('.')
+      if (n >= 0) {
+        return
+      }
+      setNumber(number + '.')
+    } else if (num === '删除') {
+      if (number.length === 1) {
+        setNumber('0')
+      } else {
+        _setNumber(number.slice(0, number.length - 1))
+      }
+    } else if (num === '清空') {
+      setNumber('0')
+    } else {
+      console.log(number)
+    }
+  }
+  return (
+    <Wrapper>
+      <div className="output">{number}</div>
+      <div className="pad clearfix" onClick={onClick}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="ok">OK</button>
+        <button className="zero">0</button>
+        <button>.</button>
+      </div>
+    </Wrapper>
+  )
+}
 export { NumberPadSection }
